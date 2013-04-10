58663c7d973cc15e7584b896de2235e99bf4c1c5
lvrach <leo.al.vra@gmail.com>
2013-04-11 00:06:28 +0300

    use iteratoration by reference loop

--- syntax.php
+++ syntax.php
@@ -109,11 +109,13 @@ class syntax_plugin_authorstats extends DokuWiki_Syntax_Plugin {
         foreach($authors as &$author) {
             $author['lmc'] = $this->_getLMC($author, $months);
         } 
+
         uasort($authors, array($this, '_sortByLMC'));
-        foreach ($authors as $author) {
+        foreach ($authors as &$author) {
             if (!empty($author['name']) and $author['lmc']) $output .= "<tr><th>" . 
                                     $author['name'] . "</th><td>" . 
                                     strval($author['lmc']) . "</td></tr>";
+            $debug .= strval($author['lmc']) . ',';
         }
         $output .= "</table>";
         return $output;
