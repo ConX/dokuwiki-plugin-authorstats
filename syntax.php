d83d8242e0b59a52d0aa247bf4eeaa5c777d4e4e
Constantinos Xanthopoulos <conx@xanthopoulos.info>
2014-08-30 18:36:07 +0300

    Updating error messages

--- syntax.php
+++ syntax.php
@@ -125,7 +125,7 @@ class syntax_plugin_authorstats extends DokuWiki_Syntax_Plugin
     {
         $output = "<h3>Yearly Contributions</h3>";
         $authors = authorstatsReadJSON()["authors"];
-        if (!$authors) return "There are no stats to output! You should generate the stats from the admin panel first.";
+        if (!$authors) return "There are no stats to output!";
         $totalpm = Array();
         $months = Array("January", "February", "March", "April","May","June","July","August","September","October", "November", "December");
         for ($i=0; $i < 12; $i++)
@@ -155,7 +155,7 @@ class syntax_plugin_authorstats extends DokuWiki_Syntax_Plugin
     {   
         $output = " <h3>General Statistics</h3><table class=\"authorstats-table\"><tr><th>Name</th><th>Creates</th><th>Edits</th><th>Minor edits</th><th>Deletes</th><th>Reverts</th><th>Contrib</th></tr>";
         $authors = authorstatsReadJSON()["authors"];
-        if (!$authors) return "There are no stats to output! You should generate the stats from the admin panel first.";
+        if (!$authors) return "There are no stats to output!";
         uasort($authors, array($this, '_sortByContrib'));
         foreach ($authors as $name => $author) 
         {
@@ -178,7 +178,7 @@ class syntax_plugin_authorstats extends DokuWiki_Syntax_Plugin
     {   
         $output = "<h3>Contribution in the last ".$months." months</h3><table class=\"authorstats-table\"><tr><th>Name</th><th>Contrib</th></tr>";
         $authors = authorstatsReadJSON()["authors"];
-        if (!$authors) return "";
+        if (!$authors) return "There are no stats to output!";
         foreach($authors as $name=>$author) 
         {
             $authors[$name]['lmc'] = $this->_getLastMonthsContrib($author, $months);
