a6d77a751ecb57f7a88700aee436fcfd1f1770ff
Constantinos Xanthopoulos <conx@xanthopoulos.info>
2014-08-30 03:43:25 +0300

    Fixing issues, implement delta updates, add graph
    - Fixing #6
    - Implement delta updates
    - Implement <AUTHORSTATS YEARGRAPH>
    - Change code style
    - Add comments

--- action.php
+++ action.php
@@ -15,79 +15,77 @@ if (!defined('DOKU_TAB')) define('DOKU_TAB', "\t");
 if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
 
 require_once DOKU_PLUGIN.'action.php';
+require_once DOKU_PLUGIN.'authorstats/helpers.php';
 
-class action_plugin_authorstats extends DokuWiki_Action_Plugin {
+class action_plugin_authorstats extends DokuWiki_Action_Plugin 
+{
 
     var $supportedModes = array('xhtml', 'metadata');
 
-    public function register(Doku_Event_Handler &$controller) {
-        $controller->register_hook('ACTION_SHOW_REDIRECT', 'BEFORE', $this, '_updateStats');
-        $controller->register_hook('PARSER_CACHE_USE','BEFORE', $this, '_cache_prepare');
+    public function register(Doku_Event_Handler &$controller) 
+    {
+        $controller->register_hook('ACTION_SHOW_REDIRECT', 'BEFORE', $this, '_updateSavedStats');
+        $controller->register_hook('PARSER_CACHE_USE','BEFORE', $this, '_cachePrepare');
     }
 
-    public function _updateStats(Doku_Event &$event, $param) {
+
+    // Updates the saved statistics by checking the last lines
+    // in the /data/meta/ directory
+    public function _updateSavedStats() 
+    {   
         global $conf;
+        $dir = $conf['metadir'] . '/';
 
-        //Check if the action was given as array key
-        if(is_array($event->data)) {
-            list($act) = array_keys($event->data);
-        } 
-        else {
-            $act = $event->data;
-        }
+        // Return the files in the directory /data/meta
+        $files = $this->_getChangeLogs($dir);
 
-        $file = $conf['metadir'] . '/_dokuwiki.changes';     
-        $lines = file($file);
-        $lastline = $lines[count($lines)-1];
+        // Read saved data from JSON file
+        $sd = authorstatsReadJSON();
 
-        if ($act == 'save' && actionOK($act)) {
-            if (act_permcheck($act) == 'save') {
-                $event->data = act_save($act);
-                if ($event->data == 'show') {
-                    $lines = file($file);
-                    $newlastline = $lines[count($lines)-1];
-                    if ($lastline != $newlastline) {    //If there is a new last line after the save, meaning there was actually an edit
-                        $parts = explode(DOKU_TAB, $newlastline);
-                        $authors = $this->_getFromFile();
-                        if (!isset($authors[$parts[4]])) {    //If the author is not in the array, initialize his stats
-                            $authors[$parts[4]]["name"] = $parts[4];
-                            $authors[$parts[4]]["C"] = 0;
-                            $authors[$parts[4]]["E"] = 0;
-                            $authors[$parts[4]]["e"] = 0;
-                            $authors[$parts[4]]["D"] = 0;
-                            $authors[$parts[4]]["R"] = 0;
-                            $authors[$parts[4]]["pm"] = Array();
-                        }
-                        // Check if we have that month in the array! 
-                        if (!isset($authors[$parts[4]]["pm"][date("Ym",$parts[0])])) {
-                            $authors[$parts[4]]["pm"][date("Ym",$parts[0])] = 1;
-                        }
-                        else {
-                            $authors[$parts[4]]["pm"][date("Ym",$parts[0])]++;
-                        }
-                        $authors[$parts[4]][$parts[2]]++; 
-                        $this->_putToFile($authors);
-                    }
+        // Get last change 
+        $lastchange = empty($sd) ?  (-1*PHP_INT_MAX )-1 : (int) $sd["lastchange"];
+        $newlast = $lastchange; 
+        foreach ($files as $file) 
+        {
+            $file_contents = array_reverse(file($file));
+            foreach($file_contents as $line)
+            {
+                $r = $this->_parseChange($line);
+                if ($r["timestamp"] <= $lastchange)
+                    break;
+
+                // Update the last if there is a more recent change
+                $newlast = max($newlast, $r["timestamp"]);
+
+                // If the author is not in the array, initialize his stats
+                if (!isset($sd["authors"][$r["author"]]))
+                {
+                    $sd["authors"][$r["author"]]["C"] = 0; 
+                    $sd["authors"][$r["author"]]["E"] = 0;
+                    $sd["authors"][$r["author"]]["e"] = 0;
+                    $sd["authors"][$r["author"]]["D"] = 0;
+                    $sd["authors"][$r["author"]]["R"] = 0;
+                    $sd["authors"][$r["author"]]["pm"] = Array(); 
                 } 
+                else
+                {
+                    // Initialize month if doesn't exist
+                    // else increment it
+                    if (!isset($sd["authors"][$r["author"]]["pm"][$r["date"]])) 
+                        $sd["authors"][$r["author"]]["pm"][$r["date"]] = 1;
+                    else 
+                        $sd["authors"][$r["author"]]["pm"][$r["date"]]++;
+                }
+                $sd["authors"][$r["author"]][$r["type"]]++; 
             }
         }
+        $sd["lastchange"] = $newlast;
+        authorstatsSaveJSON($sd);
     }
 
-    function _getFromFile() {
-        $json = new JSON(JSON_LOOSE_TYPE);
-        $file = @file_get_contents(DOKU_PLUGIN."authorstats/authorstats.json");
-        if(!$file) return false;
-        return $json->decode($file);
-    }
-
-    function _putToFile($authors) {
-        $json = new JSON();
-        $json = $json->encode($authors);
-        file_put_contents(DOKU_PLUGIN."authorstats/authorstats.json", $json); 
-    }
-
-    function _cache_prepare(&$event, $param) {    //If the page is no more recent than the modification of the json file, refresh the page.
-
+    // If the page is no more recent than the modification of the json file, refresh the page.
+    public function _cachePrepare(&$event, $param) 
+    {   
         $cache =& $event->data;
 
         if(!isset($cache->page)) return;
@@ -95,8 +93,10 @@ class action_plugin_authorstats extends DokuWiki_Action_Plugin {
 
         $enabled = p_get_metadata($cache->page, 'authorstats-enabled');
 
-        if (isset($enabled)) {
-            if (@filemtime($cache->cache) < @filemtime(DOKU_PLUGIN."authorstats/authorstats.json")) {
+        if (isset($enabled)) 
+        {
+            if (@filemtime($cache->cache) < @filemtime(DOKU_PLUGIN."authorstats/authorstats.json")) 
+            {
                 $event->preventDefault();
                 $event->stopPropagation();
                 $event->result = false;
@@ -104,6 +104,35 @@ class action_plugin_authorstats extends DokuWiki_Action_Plugin {
         }
     }
 
-}
+    function _getChangeLogs($dir, &$files = array()) 
+    {    
+        if ($dh = opendir($dir)) 
+        {
+            while (($res = readdir($dh)) !== false) 
+            {
+                if(is_dir($dir . $res . '/') && $res != '.' && $res != '..') array_merge($files, $this->_getChangeLogs($dir . $res . '/', $files));
+                else 
+                {
+                    if (strpos($res, '.changes') !== false && $res[0] != '_') $files[] = $dir . $res; 
+                }
+            } 
+            closedir($dh);
+        }
+        return $files; 
+    } 
 
+    function _parseChange($line)
+    {
+        $record = Array();
+        $parts = explode(DOKU_TAB, $line);
+        if ($parts && $parts[4] != "") 
+        {
+            $record["timestamp"] = $parts[0];
+            $record["type"] = $parts[2];
+            $record["author"] = $parts[4];
+            $record["date"] = date("Ym",$parts[0]);
+        }
+        return $record;
+    }
+}
 // vim:ts=4:sw=4:et:
--- syntax.php
+++ syntax.php
@@ -15,118 +15,183 @@ if (!defined('DOKU_TAB')) define('DOKU_TAB', "\t");
 if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
 
 require_once DOKU_PLUGIN.'syntax.php';
+require_once DOKU_PLUGIN.'authorstats/helpers.php';
+require_once DOKU_PLUGIN.'authorstats/gchart/gChartInit.php';
 
-class syntax_plugin_authorstats extends DokuWiki_Syntax_Plugin {
-    public function getType() {
+class syntax_plugin_authorstats extends DokuWiki_Syntax_Plugin 
+{
+    public function getType() 
+    {
         return 'substition';
     }
 
-    public function getPType() {
+    public function getPType() 
+    {
         return 'stack';
     } 
 
-    public function getSort() {
+    public function getSort() 
+    {
         return 371;
     } 
 
-
-    public function connectTo($mode) {
+    public function connectTo($mode) 
+    {
         $this->Lexer->addSpecialPattern('<AUTHORSTATS>',$mode,'plugin_authorstats');
         $this->Lexer->addSpecialPattern('<AUTHORSTATS [0-9]+>',$mode,'plugin_authorstats');
+        $this->Lexer->addSpecialPattern('<AUTHORSTATS YEARGRAPH>',$mode,'plugin_authorstats');
     }
 
-    public function handle($match, $state, $pos, &$handler){
+    public function handle($match, $state, $pos, &$handler)
+    {
         return array($match);
     }
 
-    public function render($mode, &$renderer, $data) {
+    public function render($mode, &$renderer, $data) 
+    {
 
-        if ($mode == "metadata") {
+        if ($mode == "metadata") 
+        {
             $renderer->meta['authorstats-enabled'] = 1;
             return true;
         }
 
-        if($mode == 'xhtml') {
-            if (preg_match("/<AUTHORSTATS (?P<months>[0-9]+)>/", $data[0], $matches)) {
-                $renderer->doc .= $this->_getMonthlyStatsTable($authors, intval($matches[1]));
+        if($mode == 'xhtml') 
+        {
+            if (preg_match("/<AUTHORSTATS (?P<months>[0-9]+)>/", $data[0], $matches)) 
+            {
+                $renderer->doc .= $this->_getMonthlyStatsTable(intval($matches[1]));
             }
-            else {
-                $renderer->doc .= $this->_getStatsTable($authors);
+            else if (preg_match("/<AUTHORSTATS YEARGRAPH>/", $data[0], $matches))
+            {
+                $renderer->doc .= $this->getYearGraph();
+            }
+            else 
+            {
+                $renderer->doc .= $this->_getStatsTable();
             }
         }
     }
 
-    // Returns the number of author's contributions for a number of months
-    function _getLMC($author, $months) {
+    // Returns the number of author's Contrib for a number of months
+    function _getLastMonthsContrib($author, $months) 
+    {
         $m = Array();
         $sum = 0;
-        for ($i=$months-1; $i>=0; $i--) { //Get an array of months in the format used eg. 201208, 201209, 201210
+        // Get an array of months in the format used eg. 201208, 201209, 201210
+        for ($i=$months-1; $i>=0; $i--) 
             array_push($m, date("Ym", strtotime("-".$i." Months")));
-        }
-        foreach ($m as $month) { // Add contributions
-            if (array_key_exists($month, $author["pm"])) {
-                $sum += $author["pm"][$month];
+        
+        // Sum the Contrib
+        foreach ($m as $month) 
+        {
+            if (array_key_exists($month, $author["pm"])) 
+            {
+                $sum += intval($author["pm"][$month]);
             }
         }
-        return $sum;  
+        return $sum;
     }
 
-    function _sortByContributions($a, $b) {
-        return intval($a["C"]) + intval($a["E"]) + intval($a["e"]) + intval($a["D"]) + intval($a["R"]) 
-        >= 
-        intval($b["C"]) + intval($b["E"]) + intval($b["e"]) + intval($b["D"]) + intval($b["R"]) 
-        ? -1 : 1;
+    function _sortByContrib($a, $b) 
+    {
+        return $this->_getTotalContrib($a) <= $this->_getTotalContrib(b) ? -1 : 1 ;
     }
 
-    function _sortByLMC($a, $b) {
-       return $a['lmc'] >= $b['lmc'] ? -1 : 1;
+    function _getTotalContrib($a)
+    {
+        return (intval($a["C"]) + intval($a["E"]) + intval($a["e"]) + intval($a["D"]) + intval($a["R"]));
+    }
+
+    function _sortByLastMonthsContrib($a, $b) 
+    {
+        return $a['lmc'] >= $b['lmc'] ? -1 : 1;
     } 
 
-    function _getStatsTable($authors) {    //Returns the HTML table with the authors and their stats
-        $output = "<h3>General Statistics</h3><table class=\"authorstats-table\"><tr><th>Name</th><th>Creates</th><th>Edits</th><th>Minor edits</th><th>Deletes</th><th>Reverts</th><th>Contributions</th></tr>";
-        $authors = $this->_getFromFile();    
+    function _getMonthlyContrib($authors, $yearmonth)
+    {
+        $sum = 0;
+        foreach ($authors as $author)
+        {
+            if (array_key_exists($yearmonth, $author["pm"])) 
+            {
+                $sum += intval($author["pm"][$yearmonth]);
+            }
+        }
+        return $sum;
+    }
+
+    function getYearGraph()
+    {
+        $output = "<h3>Yearly Contributions</h3>";
+        $authors = authorstatsReadJSON()["authors"];
         if (!$authors) return "There are no stats to output! You should generate the stats from the admin panel first.";
-        uasort($authors, array($this, '_sortByContributions'));
-        foreach ($authors as $author) {
-            if (!empty($author['name'])) $output .= "<tr><th>" . 
-                                    $author['name'] . "</th><td>" . 
-                                    $author['C'] . "</td><td>" . 
-                                    $author['E'] .  "</td><td>" . 
-                                    $author['e'] . "</td><td>" . 
-                                    $author['D'] . "</td><td>" . 
-                                    $author['R'] . "</td><td>" . 
-                                    strval(intval($author['C'])+intval($author['E'])+intval($author['e'])+intval($author['D'])+intval($author['R']))."</td></tr>";
+        $totalpm = Array();
+        $months = Array("January", "February", "March", "April","May","June","July","August","September","October", "November", "December");
+        for ($i=0; $i < 12; $i++)
+        {
+            array_push($totalpm, $this->_getMonthlyContrib($authors, date("Y").sprintf("%02s", $i))); 
         }
-        $output .= "</table>";
-        return $output;
-    }
-
-
-    function _getMonthlyStatsTable($authors, $months) {    //Returns the HTML table with the authors and their contributions for the last <$months> months
-        $output = "<h3>Contribution in the last ".$months." months</h3><table class=\"authorstats-table\"><tr><th>Name</th><th>Contributions</th></tr>";
-        $authors = $this->_getFromFile();    
-        if (!$authors) return "";
-        foreach($authors as &$author) {
-            $author['lmc'] = $this->_getLMC($author, $months);
-        } 
-        uasort($authors, array($this, '_sortByLMC'));
-        foreach ($authors as &$author) {
-            if (!empty($author['name']) and $author['lmc']) $output .= "<tr><th>" . 
-                                    $author['name'] . "</th><td>" . 
-                                    strval($author['lmc']) . "</td></tr>";
-        }
-        $output .= "</table>";
-        return $output;
-    }
-
-    function _getFromFile() {
-        $json = new JSON(JSON_LOOSE_TYPE);
-        $file = @file_get_contents(DOKU_PLUGIN."authorstats/authorstats.json");
-        if(!$file) return false;
-        return $json->decode($file);
+        $lineChart = new gchart\gLineChart(800,300);
+        $lineChart->addDataSet($totalpm);
+        $lineChart->setVisibleAxes(array('x','x', 'y','y'));
+        $lineChart->setColors(array("000000"));
+        $lineChart->addAxisLabel(0, $months);
+        $lineChart->addAxisLabel(1, Array("Months"));
+        $lineChart->addAxisLabel(3, Array("Contributions"));
+        $lineChart->addAxisRange(0, 0, 12);
+        $lineChart->addAxisRange(1, 0, 100);
+        $lineChart->addAxisRange(2, 0, max($totalpm));
+        $lineChart->addAxisRange(3, 0, 100);
+        $lineChart->addAxisLabelPositions(0, range(1,12));
+        $lineChart->addAxisLabelPositions(1, 50);
+        $lineChart->addAxisLabelPositions(3, 50);
+        // Append the parameters for the Axes Titles
+        return $output."<img src=\"".$lineChart->getUrl()."\">";
     }
     
+    // Returns the HTML table with the authors and their stats
+    function _getStatsTable() 
+    {   
+        $output = " <h3>General Statistics</h3><table class=\"authorstats-table\"><tr><th>Name</th><th>Creates</th><th>Edits</th><th>Minor edits</th><th>Deletes</th><th>Reverts</th><th>Contrib</th></tr>";
+        $authors = authorstatsReadJSON()["authors"];
+        if (!$authors) return "There are no stats to output! You should generate the stats from the admin panel first.";
+        uasort($authors, array($this, '_sortByContrib'));
+        foreach ($authors as $name => $author) 
+        {
+            $output .= "<tr><th>" . 
+            $name . "</th><td>" . 
+            $author['C'] . "</td><td>" . 
+            $author['E'] .  "</td><td>" . 
+            $author['e'] . "</td><td>" . 
+            $author['D'] . "</td><td>" . 
+            $author['R'] . "</td><td>" . 
+            strval($this->_getTotalContrib($author))."</td></tr>";
+        }
+        $output .= "</table>";
+        return $output;
+    }
 
+    // Returns the HTML table with the authors and their Contrib for the 
+    // last <$months> months
+    function _getMonthlyStatsTable($months) 
+    {   
+        $output = "<h3>Contribution in the last ".$months." months</h3><table class=\"authorstats-table\"><tr><th>Name</th><th>Contrib</th></tr>";
+        $authors = authorstatsReadJSON()["authors"];
+        if (!$authors) return "";
+        foreach($authors as $name=>$author) 
+        {
+            $authors[$name]['lmc'] = $this->_getLastMonthsContrib($author, $months);
+        } 
+        uasort($authors, array($this, '_sortByLastMonthsContrib'));
+        foreach ($authors as $name=>$author) 
+        {
+            $output .= "<tr><th>" . 
+            $name . "</th><td>" . 
+            strval($authors[$name]['lmc']) . "</td></tr>";
+        }
+        $output .= "</table>";
+        return $output;
+    }
 }
-
 // vim:ts=4:sw=4:et:
